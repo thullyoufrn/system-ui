217582d02d6c6db967c2105670ca457e14030008
Thullyo Damasceno <thullyodamasceno.3@gmail.com>
2023-09-15 20:19:57 -0300

    Tabs component added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
+import { Sidebar } from '@/components/Sidebar'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ReactNode } from 'react'
-import { Sidebar } from './components/Sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,10 +13,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className="antialiased">
       <body className={inter.className}>
         <div className="grid min-h-screen grid-cols-app">
           <Sidebar />
+
           <main className="px-4 pb-12 pt-8">{children}</main>
         </div>
       </body>
---
+++ src/components/SettingsTabs/index.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import * as Tabs from '@radix-ui/react-tabs'
+import { TabItem } from './TabItem'
+
+export function SettingsTabs() {
+  return (
+    <Tabs.Root>
+      <Tabs.List className="mt-6 flex items-center gap-4 border-b border-zinc-200">
+        <TabItem value="tab1" title="My details" />
+        <TabItem value="tab2" title="Profile" isSelected />
+        <TabItem value="tab3" title="Password" />
+        <TabItem value="tab4" title="Team" />
+        <TabItem value="tab5" title="Plan" />
+        <TabItem value="tab6" title="Billing" />
+        <TabItem value="tab7" title="Email" />
+        <TabItem value="tab8" title="Notifications" />
+        <TabItem value="tab9" title="Integrations" />
+        <TabItem value="tab10" title="API" />
+      </Tabs.List>
+    </Tabs.Root>
+  )
+}
