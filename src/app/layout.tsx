3f039d0a58fc0f119085df501c40a1346df8f9a5
Thullyo Damasceno <thullyodamasceno.3@gmail.com>
2023-09-15 22:14:44 -0300

    All form fields added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <div className="grid min-h-screen grid-cols-app">
           <Sidebar />
 
-          <main className="px-4 pb-12 pt-8">{children}</main>
+          <main className="px-8 pb-12 pt-8">{children}</main>
         </div>
       </body>
     </html>
