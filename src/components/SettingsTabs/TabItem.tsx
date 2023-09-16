b694052d5a3f286c29b0cbf7c26cdb248db8b889
Thullyo Damasceno <thullyodamasceno.3@gmail.com>
2023-09-15 21:21:10 -0300

    SettingsTabs completed

--- src/components/SettingsTabs/TabItem.tsx
+++ src/components/SettingsTabs/TabItem.tsx
@@ -1,4 +1,5 @@
 import * as Tabs from '@radix-ui/react-tabs'
+import { motion } from 'framer-motion'
 
 interface TabItemProps {
   value: string
@@ -10,12 +11,15 @@ export function TabItem({ value, title, isSelected = false }: TabItemProps) {
   return (
     <Tabs.Trigger
       value={value}
-      className="relative px-1 pb-4 text-sm font-medium text-zinc-500 hover:text-violet-700"
+      className="group relative px-1 pb-4 text-sm font-medium text-zinc-500 hover:text-violet-700 data-[state=active]:text-violet-700"
     >
       <span>{title}</span>
 
       {isSelected && (
-        <div className="absolute bottom-0 left-0 right-0 h-0.5 bg-violet-700" />
+        <motion.div
+          layoutId="activeTab"
+          className="absolute -bottom-px left-0 right-0 h-0.5 bg-violet-700"
+        />
       )}
     </Tabs.Trigger>
   )
